6f101e27c67feb01009d800dde4dc4c6ce159c4b
Ganesh Bhatta <bhattaganesh05@gmail.com>
2022-11-13 15:45:27 +0545

    Footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,9 +3,37 @@ import "./Footer.css";
 function Footer() {
   return (
     <>
-      <section className="footer section" id="footer">
-        <h1>Footer</h1>
-      </section>
+      <div class="footer">
+        <h2>Ganesh Bhatta</h2>
+        <div class="footer-social">
+          <a
+            href="https://www.linkedin.com/in/ganesh-bhatta/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="uil uil-linkedin"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/ganesh.bhatta.gb"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="uil uil-facebook"></i>
+          </a>
+          <a
+            href="https://github.com/bhattaganesh/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="uil uil-github"></i>
+          </a>
+        </div>
+      </div>
+      <div class="copyright">
+        <p>
+          All Rights Reserved &#169; Ganesh Bhatt {new Date().getFullYear()}
+        </p>
+      </div>
     </>
   );
 }
